d17742b9979b90a01129a40b9f89e92b46bb5280
Mirko Raimondo Aiello <aiello@doneref.com>
2025-09-26 10:44:58 +0200

    Add typing annotations to character and monster logic

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
+from typing import Optional
 
-from item import Weapon
-from item import RangedWeapon
 from item import Armor
+from item import Item
+from item import RangedWeapon
 from item import Shield
+from item import Weapon
 from scelta_classe import choose_class
 from scelta_razza import choose_race
 from storia import adventure
@@ -13,7 +15,7 @@ race_number = int(input("1. Umano, 2. Nano, 3. Elfo, 4. Gnomo"))
 class_number = int(input("1. Barbaro, 2. Paladino, 3. Ladro, 4. Mago"))
 selected_race = choose_race(race_number)
 selected_class = choose_class(class_number)
-selected_item = None
+selected_item: Optional[Item] = None
 if race_number == 1:
     selected_item = Weapon("Mazza", 10, 10)
 if race_number == 2:
@@ -25,7 +27,8 @@ if race_number == 4:
 character_name = input("Dimmi il nome del tuo personaggio")
 sale_price = int(random.uniform(1, 10))
 selected_character = Character(character_name, selected_race, selected_class)
-selected_character.equip_item(selected_item)
+if selected_item is not None:
+    selected_character.equip_item(selected_item)
 print(selected_character)
 while not selected_character.is_dead():
     adventure(selected_character)
